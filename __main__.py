d59f66b6ccc81d1d66d8f35cc8bf236e203cd47d
buzzi <buzzi.javier@gmail.com>
2017-04-28 19:27:14 +0200

    Sets and gets passwords, logs into workday

--- __main__.py
+++ __main__.py
@@ -1,12 +1,38 @@
+import os
+import keyring
+import getpass
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Chrome()
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element_by_name("q")
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
+
+def get_password(user):
+    """
+    Will use the OSX KeyChain to get and store your password
+    """
+    password = keyring.get_password('workday', user)
+
+    if password is None:
+        password = getpass.getpass('Password:')
+        keyring.set_password('workday', user, password)
+
+    return password
+
+
+
+user = os.getenv('USER')
+password = get_password(user)
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--incognito")
+
+driver = webdriver.Chrome(chrome_options=chrome_options)
+driver.get("https://{}:{}@sso.advisory.com/workday/login".format(user, password))
+
+# assert "Python" in driver.title
+# elem = driver.find_element_by_name("q")
+# elem.clear()
+# elem.send_keys("pycon")
+# elem.send_keys(Keys.RETURN)
+# assert "No results found." not in driver.page_source
 driver.close()
