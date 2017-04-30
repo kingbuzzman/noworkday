649b9da71e520784144414789d5814390137cedc
buzzi <buzzi.javier@gmail.com>
2017-04-30 21:39:52 +0200

    Adds logging

--- __main__.py
+++ __main__.py
@@ -3,6 +3,7 @@ import keyring
 import getpass
 import random
 import time
+import logging
 
 from decimal import Decimal
 
@@ -11,6 +12,13 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+console = logging.StreamHandler()
+console.setLevel(logging.DEBUG)
+console.setFormatter(logging.Formatter('%(message)s'))
+log.addHandler(console)
 
 
 def quarter_round(num):
@@ -24,6 +32,7 @@ def time_distribution(hours):
     """
     Generates 3 time slots based on the amount of hours
     """
+    log.info('Generating {} hours worth of data'.format(hours))
     percents = {
         'guide': 0,
         'navigate': 0,
@@ -36,16 +45,22 @@ def time_distribution(hours):
         high = (10000 - (sum(percents.values()) * 100))
         percents[key] = quarter_round(random.randrange(0, high) / 100)
 
+    log.debug('First iteration {} having a total of {} percents'.format(percents, sum(percents.values())))
+
     total_sum = sum(percents.values())
     while total_sum != 100:
         high = (100 - total_sum)
         percents[random.sample(percents.keys(), 1)[0]] += quarter_round(float(random.randrange(0, high * 100)) / 100)
         total_sum = sum(percents.values())
 
+        log.debug('Iteration yielded {} with a sum of {}'.format(percents, sum(percents.values())))
+
     distribution = {}
     minutes = float(hours) * 60
     for key, percent in percents.iteritems():
         distribution[key] = quarter_round(minutes * (float(percent) / 100) / 60)
+
+    log.debug('Created {} with a total hour count of {}'.format(distribution, sum(distribution.values())))
 
     return distribution
 
@@ -54,6 +69,9 @@ def week_distribution(min_daily_hours=8, max_daily_hours=13, days_in_week=5):
     """
     Generate a list of hours worked for an entire week
     """
+    log.info('Generating {} day(s) worth of timesheets with a minimum of {} hours and a maximum of {} hours'.format(
+        days_in_week, min_daily_hours, max_daily_hours))
+
     for _ in xrange(days_in_week):
         daily_hours = quarter_round(random.uniform(min_daily_hours, max_daily_hours))
         yield time_distribution(daily_hours)
@@ -66,6 +84,7 @@ def get_password(user):
     password = keyring.get_password('workday', user)
 
     if password is None:
+        print("Please enter your Advisory Board SSO password.")
         password = getpass.getpass('Password:')
         keyring.set_password('workday', user, password)
 
@@ -76,25 +95,22 @@ def open_submenu(driver, time_type):
     """
     Open the dialog's drop down menu
     """
+    log.debug('Opening the menu for Time Type: {}'.format(time_type))
     time_type_label = "(//label[contains(text(), 'Time Type')]/../../div)[2]/div/span/div"
     get_element(driver, time_type_label).click()
 
-    # try:
-    if True:
-        # Make selection
-        if time_type == 'admin':
-            submenu_dropdown(driver, "(//div[text() = '{}'])[last()]", 'Project Plan Tasks',
-                            'Education Advisory Board', 'All', 'Education Advisory Board > All - Admin/Other')
-        elif time_type == 'guide':
-            submenu_dropdown(driver, "(//div[text() = '{}'])[last()]", 'Project Plan Tasks',
-                             'Education Advisory Board', 'EAB',
-                             'Education Advisory Board > EAB - Guide  (01/01/2017 - 12/31/2017)')
-        elif time_type == 'navigate':
-            submenu_dropdown(driver, "(//div[text() = '{}'])[last()]", 'Project Plan Tasks',
-                             'Education Advisory Board', 'EAB',
-                             'Education Advisory Board > EAB - Navigate  (01/01/2017 - 12/31/2017)')
-    # except:
-    #     open_submenu(driver, time_type)
+    # Make selection
+    if time_type == 'admin':
+        submenu_dropdown(driver, "(//div[text() = '{}'])[last()]", 'Project Plan Tasks',
+                         'Education Advisory Board', 'All', 'Education Advisory Board > All - Admin/Other')
+    elif time_type == 'guide':
+        submenu_dropdown(driver, "(//div[text() = '{}'])[last()]", 'Project Plan Tasks',
+                         'Education Advisory Board', 'EAB',
+                         'Education Advisory Board > EAB - Guide  (01/01/2017 - 12/31/2017)')
+    elif time_type == 'navigate':
+        submenu_dropdown(driver, "(//div[text() = '{}'])[last()]", 'Project Plan Tasks',
+                         'Education Advisory Board', 'EAB',
+                         'Education Advisory Board > EAB - Navigate  (01/01/2017 - 12/31/2017)')
 
 
 def submenu_dropdown(driver, xpath_format, *menus):
@@ -102,6 +118,7 @@ def submenu_dropdown(driver, xpath_format, *menus):
     Traverse the dropdown menus
     """
     for menu in menus:
+        log.debug('Seleting submenu item: {}'.format(menu))
         xpath = xpath_format.format(menu)
         element = get_element(driver, xpath)
 
@@ -122,8 +139,6 @@ def main():
 
     driver = webdriver.Chrome(chrome_options=chrome_options)
     driver.get("https://{}:{}@sso.advisory.com/workday/login".format(user, password))
-
-    # get_element(driver, "(//div[@role = 'alertdialog']/descendant::button)[1]").click()
 
     time_icon = "//span[text() = 'Time']"
     get_element(driver, time_icon).click()
